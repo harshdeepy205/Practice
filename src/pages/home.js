0c348795728e6d815d007226f185fabcaf16eb52
harshdeepy205 <harshdeep205@outlook.com>
2020-11-18 12:41:00 +0530

    cards

--- src/pages/home.js
+++ src/pages/home.js
@@ -25,7 +25,7 @@ function Home() {
                 <div className="card post-card">
                     <h4 className="card-title">Card Title</h4>
                     <div className="card-image">
-                        <img src="https://images.unsplash.com/photo-1591213595166-82c13a93b1c8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" />
+                        <img src="https://images.unsplash.com/photo-1502759683299-cdcd6974244f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" />
                     </div>
                     <div className="card-action mx-2 my-2">
                         <a href="#" >This is a link</a>
@@ -34,11 +34,43 @@ function Home() {
                     <div className="my-2 mx-2 card-content">
                         <h6>Title</h6>
                         <p>Nature's Beauty</p>
-                        <input type="text" placeholder="Add a comment" style={{ border: "none", textDecoration: "none" }} />
+                        <input type="text" className="form-control" placeholder="Add a comment" style={{ border: "none", textDecoration: "none" }} />
+                    </div>
+                </div>
+
+                <div className="card post-card my-2">
+                    <h4 className="card-title">Card Title</h4>
+                    <div className="card-image">
+                        <img src="https://images.unsplash.com/photo-1502759683299-cdcd6974244f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" />
+                    </div>
+                    <div className="card-action mx-2 my-2">
+                        <a href="#" >This is a link</a>
+                        <a href="#" className="mx-4">This is a link</a>
+                    </div>
+                    <div className="my-2 mx-2 card-content">
+                        <h6>Title</h6>
+                        <p>Nature's Beauty</p>
+                        <input type="text" className="form-control" placeholder="Add a comment" style={{ border: "none", textDecoration: "none" }} />
+                    </div>
+                </div>
+
+                <div className="card post-card">
+                    <h4 className="card-title">Card Title</h4>
+                    <div className="card-image">
+                        <img src="https://images.unsplash.com/photo-1502759683299-cdcd6974244f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" />
+                    </div>
+                    <div className="card-action mx-2 my-2">
+                        <a href="#" >This is a link</a>
+                        <a href="#" className="mx-4">This is a link</a>
+                    </div>
+                    <div className="my-2 mx-2 card-content">
+                        <h6>Title</h6>
+                        <p>Nature's Beauty</p>
+                        <input type="text" className="form-control" placeholder="Add a comment" style={{ border: "none", textDecoration: "none" }} />
                     </div>
                 </div>
             </div>
-            {/* </div> */}
+
         </>
     )
 }
