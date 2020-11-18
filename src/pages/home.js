703f6c643aabc2ef92df1868375840bc035facf9
harshdeepy205 <harshdeep205@outlook.com>
2020-11-18 18:42:20 +0530

    logo fix

--- src/pages/home.js
+++ src/pages/home.js
@@ -11,7 +11,7 @@ function Home() {
                 <div className="nav_bar">
                     <div className="logo">
                         {/* <img class="brand-logo" src="logo.PNG" alt="Not Found" /> */}
-                        <h3 class="brand-logo mxy-5" >Instagram</h3>
+                        <h3 class="brand-logo mxy-5" >🅸🅽🆂🆃🅰🅶🆁🅰🅼</h3>
                     </div>
                     <div className="menu_items">
                         <li><a href="#" className="a mx-4">Home</a></li>
